5229767e50b2bc0eb197688225ae3679bd6932be
Nông Thanh Chiến <chiennt2@rikkeisoft.com>
2023-10-05 16:58:19 +0700

    Creating objects from classes

--- Engine/Player.cs
+++ Engine/Player.cs
@@ -2,8 +2,12 @@
 
 namespace Engine
 {
-    public class Class1
+    public class Player
     {
-
+        public int CurrentHitPoints { get; set; }
+        public int MaximumHitPoints { get; set; }
+        public int Gold { get; set; }
+        public int ExperiencePoints { get; set; }
+        public int Level { get; set; }
     }
 }
