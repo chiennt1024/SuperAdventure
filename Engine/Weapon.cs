971f0f2839559f52c37d85553722a65f54c54a4f
Nông Thanh Chiến <chiennt2@rikkeisoft.com>
2023-10-05 17:23:31 +0700

    Inheritance and base classes

--- Engine/Weapon.cs
+++ Engine/Weapon.cs
@@ -4,11 +4,8 @@ using System.Text;
 
 namespace Engine
 {
-    internal class Weapon
+    internal class Weapon : Item
     {
-        public int ID { get; set; }
-        public string Name { get; set; }
-        public string NamePlural { get; set; }
         public int MinimumDamage { get; set; }
         public int MaximumDamage { get; set; }
     }
