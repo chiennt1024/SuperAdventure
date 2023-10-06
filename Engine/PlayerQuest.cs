396485aba9e768faf4f2a4ee80416c3111dcf8f3
Nông Thanh Chiến <chiennt2@rikkeisoft.com>
2023-10-06 16:15:41 +0700

    Creating collections of objects

---
+++ Engine/PlayerQuest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine
+{
+    public class PlayerQuest
+    {
+        public Quest Details { get; set; }
+        public bool IsCompleted { get; set; }
+        public PlayerQuest(Quest details)
+        {
+            Details = details;
+            IsCompleted = false;
+        }
+    }
+}
